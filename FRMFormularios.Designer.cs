77272a747cc6a516c2f132c67682d740c873195b
celso.saraujo <celso.saraujo@hotmail.com>
2026-01-30 19:26:25 -0300

    Criação do formulário principal

---
+++ FRMFormularios.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace frmTesteGitForms
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            mniFomularios = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mniFomularios });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // mniFomularios
+            // 
+            mniFomularios.Name = "mniFomularios";
+            mniFomularios.Size = new Size(82, 20);
+            mniFomularios.Text = "Formulários";
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmPrincipal";
+            Text = "Formulários Criativos";
+            WindowState = FormWindowState.Minimized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mniFomularios;
+    }
+}
---
+++ FRMFormularios.cs
@@ -0,0 +1,15 @@
+namespace frmTesteGitForms
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void celsoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
