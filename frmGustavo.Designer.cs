722e6f9f1b224c1aae2d4f668dd4e6d0460f82bb
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2026-01-30 21:36:34 -0300

    Estilização da calculadora

--- frmGustavo.Designer.cs
+++ frmGustavo.Designer.cs
@@ -43,110 +43,132 @@
             // 
             // btnSoma
             // 
+            btnSoma.BackColor = Color.DodgerBlue;
+            btnSoma.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnSoma.ForeColor = Color.White;
             btnSoma.Location = new Point(48, 224);
             btnSoma.Name = "btnSoma";
             btnSoma.Size = new Size(160, 64);
-            btnSoma.TabIndex = 0;
+            btnSoma.TabIndex = 3;
             btnSoma.Text = "Soma";
-            btnSoma.UseVisualStyleBackColor = true;
+            btnSoma.UseVisualStyleBackColor = false;
             btnSoma.Click += btnSoma_Click;
             // 
             // btnSubtracao
             // 
+            btnSubtracao.BackColor = Color.DodgerBlue;
+            btnSubtracao.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSubtracao.ForeColor = Color.White;
             btnSubtracao.Location = new Point(232, 224);
             btnSubtracao.Name = "btnSubtracao";
             btnSubtracao.Size = new Size(160, 64);
-            btnSubtracao.TabIndex = 1;
+            btnSubtracao.TabIndex = 4;
             btnSubtracao.Text = "Subtração";
-            btnSubtracao.UseVisualStyleBackColor = true;
+            btnSubtracao.UseVisualStyleBackColor = false;
             btnSubtracao.Click += btnSubtracao_Click;
             // 
             // btnMultiplicacao
             // 
+            btnMultiplicacao.BackColor = Color.DodgerBlue;
+            btnMultiplicacao.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnMultiplicacao.ForeColor = Color.White;
             btnMultiplicacao.Location = new Point(48, 304);
             btnMultiplicacao.Name = "btnMultiplicacao";
             btnMultiplicacao.Size = new Size(160, 64);
-            btnMultiplicacao.TabIndex = 2;
+            btnMultiplicacao.TabIndex = 5;
             btnMultiplicacao.Text = "Multiplicação";
-            btnMultiplicacao.UseVisualStyleBackColor = true;
+            btnMultiplicacao.UseVisualStyleBackColor = false;
             btnMultiplicacao.Click += btnMultiplicacao_Click;
             // 
             // btnDivisao
             // 
+            btnDivisao.BackColor = Color.DodgerBlue;
+            btnDivisao.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnDivisao.ForeColor = Color.White;
             btnDivisao.Location = new Point(232, 304);
             btnDivisao.Name = "btnDivisao";
             btnDivisao.Size = new Size(160, 64);
-            btnDivisao.TabIndex = 3;
+            btnDivisao.TabIndex = 6;
             btnDivisao.Text = "Divisão";
-            btnDivisao.UseVisualStyleBackColor = true;
+            btnDivisao.UseVisualStyleBackColor = false;
             btnDivisao.Click += btnDivisao_Click;
             // 
             // txtNum1
             // 
             txtNum1.Font = new Font("Segoe UI", 16F);
-            txtNum1.Location = new Point(64, 88);
+            txtNum1.Location = new Point(56, 88);
             txtNum1.Name = "txtNum1";
-            txtNum1.Size = new Size(80, 36);
-            txtNum1.TabIndex = 4;
+            txtNum1.Size = new Size(88, 36);
+            txtNum1.TabIndex = 1;
             // 
             // txtNum2
             // 
             txtNum2.Font = new Font("Segoe UI", 16F);
-            txtNum2.Location = new Point(296, 88);
+            txtNum2.Location = new Point(304, 88);
             txtNum2.Name = "txtNum2";
-            txtNum2.Size = new Size(80, 36);
-            txtNum2.TabIndex = 5;
+            txtNum2.Size = new Size(88, 36);
+            txtNum2.TabIndex = 2;
             // 
             // txtResultado
             // 
             txtResultado.Enabled = false;
             txtResultado.Font = new Font("Segoe UI", 16F);
-            txtResultado.Location = new Point(184, 160);
+            txtResultado.Location = new Point(176, 160);
             txtResultado.Name = "txtResultado";
-            txtResultado.Size = new Size(80, 36);
+            txtResultado.Size = new Size(104, 36);
             txtResultado.TabIndex = 6;
             // 
             // lblNum1
             // 
             lblNum1.AutoSize = true;
-            lblNum1.Location = new Point(72, 64);
+            lblNum1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblNum1.ForeColor = Color.White;
+            lblNum1.Location = new Point(56, 64);
             lblNum1.Name = "lblNum1";
-            lblNum1.Size = new Size(60, 15);
+            lblNum1.Size = new Size(86, 21);
             lblNum1.TabIndex = 7;
             lblNum1.Text = "Número 1";
             // 
             // lblNum2
             // 
             lblNum2.AutoSize = true;
+            lblNum2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblNum2.ForeColor = Color.White;
             lblNum2.Location = new Point(304, 64);
             lblNum2.Name = "lblNum2";
-            lblNum2.Size = new Size(60, 15);
+            lblNum2.Size = new Size(86, 21);
             lblNum2.TabIndex = 8;
             lblNum2.Text = "Número 2";
             // 
             // lblResultado
             // 
             lblResultado.AutoSize = true;
-            lblResultado.Location = new Point(192, 136);
+            lblResultado.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblResultado.ForeColor = Color.White;
+            lblResultado.Location = new Point(184, 136);
             lblResultado.Name = "lblResultado";
-            lblResultado.Size = new Size(59, 15);
+            lblResultado.Size = new Size(86, 21);
             lblResultado.TabIndex = 9;
             lblResultado.Text = "Resultado";
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(176, 392);
+            btnClear.BackColor = Color.DodgerBlue;
+            btnClear.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnClear.ForeColor = Color.White;
+            btnClear.Location = new Point(168, 392);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(75, 23);
-            btnClear.TabIndex = 10;
+            btnClear.Size = new Size(104, 32);
+            btnClear.TabIndex = 7;
             btnClear.Text = "Apagar";
-            btnClear.UseVisualStyleBackColor = true;
+            btnClear.UseVisualStyleBackColor = false;
             btnClear.Click += btnClear_Click;
             // 
             // frmGustavo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkOrange;
             ClientSize = new Size(446, 450);
             Controls.Add(btnClear);
             Controls.Add(lblResultado);
@@ -159,8 +181,9 @@
             Controls.Add(btnMultiplicacao);
             Controls.Add(btnSubtracao);
             Controls.Add(btnSoma);
+            ForeColor = SystemColors.ControlText;
             Name = "frmGustavo";
-            Text = "frmGustavo";
+            Text = "Gustavo";
             ResumeLayout(false);
             PerformLayout();
         }
