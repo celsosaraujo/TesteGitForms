044bc5252684586d495952cf6a3f931fe28c58eb
Suely Ykemoto <suelyykemoto65@gmail.com>
2026-02-02 22:11:14 -0300

    Correção do designer do Formulário Suely

--- frmSuely.Designer.cs
+++ frmSuely.Designer.cs
@@ -56,7 +56,7 @@
             lblNome.BackColor = Color.Transparent;
             lblNome.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNome.ForeColor = SystemColors.ButtonHighlight;
-            lblNome.Location = new Point(88, 408);
+            lblNome.Location = new Point(96, 392);
             lblNome.Name = "lblNome";
             lblNome.Size = new Size(49, 17);
             lblNome.TabIndex = 0;
@@ -67,7 +67,7 @@
             lblAvaliacao.BackColor = Color.Transparent;
             lblAvaliacao.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblAvaliacao.ForeColor = SystemColors.ButtonHighlight;
-            lblAvaliacao.Location = new Point(168, 120);
+            lblAvaliacao.Location = new Point(168, 112);
             lblAvaliacao.Name = "lblAvaliacao";
             lblAvaliacao.Size = new Size(432, 32);
             lblAvaliacao.TabIndex = 1;
@@ -76,7 +76,7 @@
             // txtNome
             // 
             txtNome.BackColor = SystemColors.GradientActiveCaption;
-            txtNome.Location = new Point(144, 408);
+            txtNome.Location = new Point(152, 392);
             txtNome.MaxLength = 100;
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(432, 23);
@@ -147,7 +147,7 @@
             grpAvaliacao.Controls.Add(rdbRegular);
             grpAvaliacao.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             grpAvaliacao.ForeColor = SystemColors.ButtonHighlight;
-            grpAvaliacao.Location = new Point(88, 168);
+            grpAvaliacao.Location = new Point(88, 152);
             grpAvaliacao.Name = "grpAvaliacao";
             grpAvaliacao.Size = new Size(608, 56);
             grpAvaliacao.TabIndex = 10;
@@ -158,11 +158,11 @@
             // 
             chkProfessores.AutoSize = true;
             chkProfessores.BackColor = Color.Transparent;
-            chkProfessores.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkProfessores.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             chkProfessores.ForeColor = SystemColors.ButtonHighlight;
-            chkProfessores.Location = new Point(88, 272);
+            chkProfessores.Location = new Point(112, 272);
             chkProfessores.Name = "chkProfessores";
-            chkProfessores.Size = new Size(97, 21);
+            chkProfessores.Size = new Size(98, 21);
             chkProfessores.TabIndex = 11;
             chkProfessores.Text = "Professores";
             chkProfessores.UseVisualStyleBackColor = false;
@@ -171,9 +171,9 @@
             // 
             chkConteudo.AutoSize = true;
             chkConteudo.BackColor = Color.Transparent;
-            chkConteudo.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkConteudo.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             chkConteudo.ForeColor = SystemColors.ButtonHighlight;
-            chkConteudo.Location = new Point(216, 272);
+            chkConteudo.Location = new Point(240, 272);
             chkConteudo.Name = "chkConteudo";
             chkConteudo.Size = new Size(87, 21);
             chkConteudo.TabIndex = 12;
@@ -184,11 +184,11 @@
             // 
             chkMaterial.AutoSize = true;
             chkMaterial.BackColor = Color.Transparent;
-            chkMaterial.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkMaterial.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             chkMaterial.ForeColor = SystemColors.ButtonHighlight;
-            chkMaterial.Location = new Point(320, 272);
+            chkMaterial.Location = new Point(360, 272);
             chkMaterial.Name = "chkMaterial";
-            chkMaterial.Size = new Size(76, 21);
+            chkMaterial.Size = new Size(78, 21);
             chkMaterial.TabIndex = 13;
             chkMaterial.Text = "Material";
             chkMaterial.UseVisualStyleBackColor = false;
@@ -197,11 +197,11 @@
             // 
             chkAmbiente.AutoSize = true;
             chkAmbiente.BackColor = Color.Transparent;
-            chkAmbiente.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkAmbiente.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             chkAmbiente.ForeColor = SystemColors.ButtonHighlight;
-            chkAmbiente.Location = new Point(416, 272);
+            chkAmbiente.Location = new Point(464, 272);
             chkAmbiente.Name = "chkAmbiente";
-            chkAmbiente.Size = new Size(86, 21);
+            chkAmbiente.Size = new Size(87, 21);
             chkAmbiente.TabIndex = 14;
             chkAmbiente.Text = "Ambiente";
             chkAmbiente.UseVisualStyleBackColor = false;
@@ -210,9 +210,9 @@
             // 
             chkOutros.AutoSize = true;
             chkOutros.BackColor = Color.Transparent;
-            chkOutros.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            chkOutros.ForeColor = SystemColors.Control;
-            chkOutros.Location = new Point(520, 272);
+            chkOutros.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkOutros.ForeColor = SystemColors.HighlightText;
+            chkOutros.Location = new Point(576, 272);
             chkOutros.Name = "chkOutros";
             chkOutros.Size = new Size(69, 21);
             chkOutros.TabIndex = 15;
@@ -222,9 +222,9 @@
             // label1
             // 
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(88, 240);
+            label1.Location = new Point(88, 232);
             label1.Name = "label1";
             label1.Size = new Size(120, 16);
             label1.TabIndex = 16;
@@ -234,7 +234,7 @@
             // 
             btnLimpar.BackColor = Color.Bisque;
             btnLimpar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnLimpar.Location = new Point(680, 40);
+            btnLimpar.Location = new Point(672, 40);
             btnLimpar.Name = "btnLimpar";
             btnLimpar.Size = new Size(96, 32);
             btnLimpar.TabIndex = 17;
@@ -247,7 +247,7 @@
             btnEnviar.BackColor = Color.CornflowerBlue;
             btnEnviar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnEnviar.ForeColor = Color.Black;
-            btnEnviar.Location = new Point(672, 400);
+            btnEnviar.Location = new Point(672, 384);
             btnEnviar.Name = "btnEnviar";
             btnEnviar.Size = new Size(99, 32);
             btnEnviar.TabIndex = 18;
@@ -261,7 +261,7 @@
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe Print", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(184, 32);
+            label2.Location = new Point(184, 16);
             label2.Name = "label2";
             label2.Size = new Size(414, 62);
             label2.TabIndex = 19;
